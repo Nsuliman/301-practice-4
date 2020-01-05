a4c5765a233f0ecc2882994b8bacf6d0d7cb6435
Nsuliman <nsuliman88@gmail.com>
2020-01-05 16:46:28 +0200

    2nd feature Done, each properties

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 'use strict';
 
 // For testing 
+console.log(' ---------------------------- Testing ----------------------------------------'); 
 console.log(' Hello All in Our Node.js \n\n');
 
-//Read in the contents of a.json file with the contents given below
+
+//1.Read in the contents of a.json file with the contents given below
+console.log(' ---------------------------- 1st Feature-Read JSON File ----------------------------------------'); 
 let fileJson = require('./a.json')
-console.log(fileJson);
+console.log('fileJson : ',fileJson);
 
 
+//2.Output a list of each property of the object and it's value, separated by a colon.
+console.log(' ---------------------------- 2nd Feature-Output Each Properties ---------------------------------'); 
+let jsonToArr = [fileJson];
+// console.log('jsonToArr : ', jsonToArr);
 
+jsonToArr.forEach(eachProp =>
+    {
+        console.log('name : ', eachProp.name);
+        console.log('pets : ', eachProp.pets);
+        console.log('childern : zach :' , eachProp.children.zach.age , '      allie :',eachProp.children.allie.age );
+        console.log('job : ', eachProp.job);
+    });
 
