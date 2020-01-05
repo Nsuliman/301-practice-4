f9629ca140bcc5e6799ee937a84b9a00caf067fa
Nsuliman <nsuliman88@gmail.com>
2020-01-05 03:23:53 +0200

    1st feature Done , read JSON data

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 'use strict';
 
 // For testing 
-console.log(' Hello All in Our Node.js');
+console.log(' Hello All in Our Node.js \n\n');
 
+//Read in the contents of a.json file with the contents given below
 let fileJson = require('./a.json')
 console.log(fileJson);
+
+
 
 
